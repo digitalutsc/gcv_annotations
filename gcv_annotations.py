3b61b1d3d3e6f6542b39d09e12fd30c7a64a539a
Amy Li <106131209+amym-li@users.noreply.github.com>
2023-01-17 18:06:27 -0500

    Update @id links

--- gcv_annotations.py
+++ gcv_annotations.py
@@ -22,7 +22,7 @@ def main():
 
     # store the context, id, and type
     out_json["@context"] = "http://iiif.io/api/presentation/2/context.json"
-    out_json["@id"] = "https://islandora.traefik.me/" + OUTPUT_PATH
+    out_json["@id"] = "https://core.digital.utsc.utoronto.ca/" + OUTPUT_PATH
     out_json["@type"] = "sc:AnnotationList"
 
     # create list to store the annotations
@@ -57,14 +57,10 @@ def main():
                         # add a space between words if break detected
                         if "property" in symbol and symbol["property"]["detectedBreak"]["type"] == "SPACE":
                             content += " "
-                
-                # bind to any free port
-                sock = socket.socket()
-                sock.bind(('', 0))
 
                 # add the information retrieved to the resources dictionary
                 resources = {}
-                resources["@id"] = "http://localhost:" + str(sock.getsockname()[1]) + "/annotation/" + str(hash(json.dumps(paragraph, sort_keys=True)))
+                resources["@id"] = "https://dsu-data.utsc.utoronto.ca:8443/simpleAnnotationStore/annotation/" + str(hash(json.dumps(paragraph, sort_keys=True)))
                 resources["@type"] = "oa:Annotation"
                 resources["@context"] = "http://iiif.io/api/presentation/2/context.json"
                 resources["on"] = CANVAS + "#xywh=" + str(x0) + "," + str(y0) + "," + str(width) + "," + str(height)
